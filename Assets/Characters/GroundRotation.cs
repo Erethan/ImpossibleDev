e17859cd7a67eb6236c5c05bc2f3464ea863893a
Eric Marchetti <ericmsbr@hotmail.com>
2021-01-11 17:47:15 -0300

    Updated avatar's rotation behaviour

--- Assets/Characters/GroundRotation.cs
+++ Assets/Characters/GroundRotation.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class GroundRotation : MonoBehaviour
 {
 
-    [Tooltip("Ground component that checks wether the player is grounded and can rotate.")]
-    [SerializeField] protected Grounding grounding = default;
+    [Tooltip("Checks wether the player is grounded and can rotate.")]
+    [SerializeField] protected Grounding _grounding = default;
+
 
     [Tooltip("Rotation speed if the rotation size is less than 90 degrees.")]
     [SerializeField] protected float _rotationSpeed = 1;
 
     [Tooltip("Time in seconds that this scripts becomes idle (stops rotating) after receiving no input.")]
     [SerializeField] protected float _secondsToIdle = 0.5f;
+    
+    [Tooltip("Rotates towards movement input when no rotation input is provided.")]
+    [SerializeField] protected GroundMovement _movement = default;
 
     protected float _lastInputTime;
     public bool IgnoreWhenIdle { get; set; } = true;
@@ -42,10 +46,10 @@ public class GroundRotation : MonoBehaviour
         get
         {
 
-            float rotationAngle = Quaternion.Angle(grounding.Rigidbody.rotation, _rotationTarget);
+            float rotationAngle = Quaternion.Angle(_grounding.Rigidbody.rotation, _rotationTarget);
             return rotationAngle >= 90
                 ? _rotationTarget
-                : Quaternion.RotateTowards(grounding.Rigidbody.rotation, _rotationTarget, rotationAngle * _rotationSpeed);
+                : Quaternion.RotateTowards(_grounding.Rigidbody.rotation, _rotationTarget, rotationAngle * _rotationSpeed);
         }
     }
 
@@ -53,26 +57,35 @@ public class GroundRotation : MonoBehaviour
     protected void FixedUpdate()
     {
 
-        if (grounding.Grounded)
+        if (_grounding.Grounded)
         {
-            if(IgnoreWhenIdle 
-                && Time.time > _lastInputTime + _secondsToIdle)
+            bool isIdle = Time.time > _lastInputTime + _secondsToIdle;
+            bool hasRotationInput = _rotationInput.sqrMagnitude > float.Epsilon;
+            bool hasMovementInput = _movement.MovementInput.sqrMagnitude > float.Epsilon;
+
+            if (IgnoreWhenIdle 
+                && isIdle)
             {
-                if (grounding.Rigidbody.velocity.sqrMagnitude > 0.1f && _rotationInput.sqrMagnitude < float.Epsilon)
+                if (_grounding.Rigidbody.velocity.sqrMagnitude > float.Epsilon && !hasRotationInput)
                 {
-                    //TODO: Get movement from Input
-                    float movementAngle = Vector3.SignedAngle(Vector3.forward, grounding.Rigidbody.velocity, Vector3.up);
-                    _rotationTarget = AngleToRotation(movementAngle - 90);
+                    float movementAngle = Mathf.Atan2(_movement.MovementInput.y, _movement.MovementInput.x) * Mathf.Rad2Deg;
+                    _rotationTarget = AngleToRotation(90 - movementAngle);
                 }
             }
 
-            grounding.Rigidbody.MoveRotation(RotationStep);
+            
+            if( (!isIdle && hasRotationInput)
+                || (isIdle && hasMovementInput))
+            {
+                _grounding.Rigidbody.MoveRotation(RotationStep);
+            }
+
         }
     }
 
     protected Quaternion AngleToRotation(float angle)
     {
-        Vector3 referenceUp = Vector3.ProjectOnPlane(grounding.DirectionReference.up, Vector3.up);
+        Vector3 referenceUp = Vector3.ProjectOnPlane(_grounding.DirectionReference.up, Vector3.up);
         angle += Vector3.SignedAngle(Vector3.forward, referenceUp, Vector3.up);
 
         //TODO: Account for grounding.NormalAxis
--- Assets/Input/ImpossibleDev.cs
+++ Assets/Input/ImpossibleDev.cs
@@ -57,7 +57,7 @@ public class @ImpossibleDev : IInputActionCollection, IDisposable
                     ""id"": ""978bfe49-cc26-4a3d-ab7b-7d7a29327403"",
                     ""path"": ""<Gamepad>/leftStick"",
                     ""interactions"": """",
-                    ""processors"": """",
+                    ""processors"": ""StickDeadzone"",
                     ""groups"": "";Gamepad"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
