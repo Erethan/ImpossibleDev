ee9c5fa0eac015179f508ba08b7e397712ef965c
Eric Marchetti <ericmsbr@hotmail.com>
2021-02-22 17:49:54 -0300

    prapraring packages to be exported from project

--- Assets/Scripts/LoadingScreenProgress.cs
+++ Assets/Scripts/LoadingScreenProgress.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pulse;
+using Erethan.SceneLoadingSystem;
 
 public class LoadingScreenProgress : MonoBehaviour
 {
